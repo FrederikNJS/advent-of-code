4d65bf8033452734dcdfe2ae566e5454c1d1e3c9
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2021-12-13 23:16:44 +0100

    Add Day 11

--- ruby/2021/11/part1.rb
+++ ruby/2021/11/part1.rb
@@ -1,7 +1,3 @@
-10
-10
-10
-10
 require 'pry'
 
 module Y2021
@@ -12,7 +8,48 @@ module Y2021
             end
 
             def self.parse_input raw
-                raw.map {|line| line.chars.map{|char| char.to_i}}
+                self.grid_to_map(raw.map {|line| line.chars.map{|char| char.to_i}})
+            end
+
+            def self.grid_to_map grid
+                hash = Hash.new
+                grid.each_with_index do |row, row_idx|
+                    row.each_with_index do |cell, col_idx|
+                        hash[[row_idx, col_idx]] = cell
+                    end
+                end
+                hash
+            end
+
+            def self.step input
+                incremented = input.map {|coord, value| [coord, value+1]}.to_h
+                queue = Queue.new
+                incremented.filter {|coord, value| value >= 10}.each {|coord, value| queue << coord}
+                flashers = Set.new
+                until queue.empty?
+                    flasher = queue.pop
+                    flashers << flasher
+                    x, y = flasher
+                    neighbors = [
+                        [x-1, y-1],
+                        [x-1, y],
+                        [x-1, y+1],
+                        [x, y-1],
+                        [x, y+1],
+                        [x+1, y-1],
+                        [x+1, y],
+                        [x+1, y+1]
+                    ]
+                    neighbors.each do |coord|
+                        if incremented.has_key?(coord) && !flashers.include?(coord)
+                            incremented[coord] += 1
+                            if incremented[coord] == 10
+                                queue << coord
+                            end
+                        end
+                    end
+                end
+                [incremented.map {|coord, value| [coord, value >= 10 ? 0 : value]}.to_h, flashers.size]
             end
         end
     end
--- ruby/spec/2021/day11_spec.rb
+++ ruby/spec/2021/day11_spec.rb
@@ -12,14 +12,67 @@ RSpec.describe '2021 Day 11' do
         end
 
         context 'examples' do
-            it 'matches the numbers in the first example' do
+            it 'matches the numbers in the first' do
+                input = part1.grid_to_map([
+                    [1,1,1,1,1],
+                    [1,9,9,9,1],
+                    [1,9,1,9,1],
+                    [1,9,9,9,1],
+                    [1,1,1,1,1]
+                ])
+                after_1_step, flash_count = part1.step(input)
+                expect(after_1_step).to eq part1.grid_to_map([
+                    [3,4,5,4,3],
+                    [4,0,0,0,4],
+                    [5,0,0,0,5],
+                    [4,0,0,0,4],
+                    [3,4,5,4,3]
+                ])
+                expect(flash_count).to eq 9
 
+                after_2_steps, flash_count = part1.step(after_1_step)
+                expect(after_2_steps).to eq part1.grid_to_map([
+                    [4,5,6,5,4],
+                    [5,1,1,1,5],
+                    [6,1,1,1,6],
+                    [5,1,1,1,5],
+                    [4,5,6,5,4]
+                ])
+                expect(flash_count).to eq 0
+            end
+
+            it 'matches the numbers in the second example' do
+                raw_example = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526".lines.map(&:chomp)
+                input = part1.parse_input raw_example
+                flash_count = 0
+                100.times do
+                    input, flashes = part1.step input
+                    flash_count += flashes
+                end
+                expect(flash_count).to eq 1656
             end
         end
 
         context "challenge" do
             it "finds the answer" do
-
+                raw = part1.read_input
+                input = part1.parse_input raw
+                flash_count = 0
+                100.times do
+                    input, flashes = part1.step input
+                    flash_count += flashes
+                end
+                expect(flash_count).to be < 102520
+                expect(flash_count).to eq 1691
             end
         end
     end
@@ -31,13 +84,44 @@ RSpec.describe '2021 Day 11' do
 
         context "examples" do
             it 'matches the numbers in the example' do
-
+                raw_example = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526".lines.map(&:chomp)
+                input = part1.parse_input raw_example
+                winning_round = -1
+                200.times do |round|
+                    input, flashes = part1.step input
+                    if flashes == input.size
+                        winning_round = round + 1
+                        break
+                    end
+                end
+                expect(winning_round).to eq 195
             end
         end
 
         context "challenge" do
             it "finds the answer" do
+                raw = part1.read_input
 
+                input = part1.parse_input raw
+                winning_round = -1
+                1000.times do |round|
+                    input, flashes = part1.step input
+                    if flashes == input.size
+                        winning_round = round + 1
+                        break
+                    end
+                end
+                expect(winning_round).to be > 215
+                expect(winning_round).to eq 216
             end
         end
     end
