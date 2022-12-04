64f1d12f6f8414972664121edc45c39b18a0c681
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2022-12-04 11:59:37 +0100

    Fix 2019 Day 09

--- ruby/spec/2019/09/part1_spec.rb
+++ ruby/spec/2019/09/part1_spec.rb
@@ -2,9 +2,9 @@ require_relative '../../../2019/09/part1.rb'
 
 RSpec.describe '2019/09/Part1' do
     part1 = Y2019::Day9::Part1
-    xit 'it works on the first input' do
+    it 'it works on the first input' do
         program = 109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99
-        result = part1.process_program091(program, [])
+        result = part1.process_program091(program.clone, [])
         expect(result).to eq program
     end
 end
